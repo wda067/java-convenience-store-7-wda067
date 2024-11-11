00bb9f0299466b048360f88934551d8d2a5daf68
Jo Myeong-geun <happy63590@gmail.com>
2024-11-11 17:13:55 +0900

    feat: 상품명, 수량 추출 기능

---
+++ src/main/java/store/util/Validator.java
@@ -0,0 +1,23 @@
+package store.util;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Validator {
+
+    public static Map<String, Integer> validateProduct(String input) {
+        Map<String, Integer> map = new HashMap<>();
+        Pattern pattern = Pattern.compile("\\[(.+?)-(\\d+)]");
+        Matcher matcher = pattern.matcher(input);
+
+        while (matcher.find()) {
+            String product = matcher.group(1);
+            int quantity = Integer.parseInt(matcher.group(2));
+            map.put(product, quantity);
+        }
+
+        return map;
+    }
+}
