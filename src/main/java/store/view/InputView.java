1ee46c0813f9b004c6c845576a0fa865a1c06fd4
Jo Myeong-geun <happy63590@gmail.com>
2024-11-11 19:31:23 +0900

    feat: 프로모션 재고가 부족할 경우 나머지 수량 정가 결제 여부 입력 기능

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -18,4 +18,9 @@ public class InputView {
         System.out.println("현재 " + item + "은(는) " + count + "개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)");
         return readLine();
     }
+
+    public String confirmPurchaseWithoutPromotion(String item, int count) {
+        System.out.println("현재 " + item + " " + count + "개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)");
+        return readLine();
+    }
 }
