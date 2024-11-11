10b6105287875c61209bfcb4ee7eaa877a13142f
Jo Myeong-geun <happy63590@gmail.com>
2024-11-11 17:13:02 +0900

    feat: 상품명, 수량 입력 기능

---
+++ src/main/java/store/view/InputView.java
@@ -0,0 +1,11 @@
+package store.view;
+
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+public class InputView {
+
+    public String inputProduct() {
+        System.out.println("\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
+        return readLine();
+    }
+}
