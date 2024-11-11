22132eaa320988b83b8b21cc21705dc992400bb2
Jo Myeong-geun <happy63590@gmail.com>
2024-11-11 19:29:57 +0900

    feat: 멤버십 적용 여부 입력 기능

--- src/main/java/store/view/InputView.java
+++ src/main/java/store/view/InputView.java
@@ -8,4 +8,9 @@ public class InputView {
         System.out.println("\n구매하실 상품명과 수량을 입력해 주세요. (예: [사이다-2],[감자칩-1])");
         return readLine();
     }
+
+    public String confirmMembership() {
+        System.out.println("멤버십 할인을 받으시겠습니까? (Y/N)");
+        return readLine();
+    }
 }
