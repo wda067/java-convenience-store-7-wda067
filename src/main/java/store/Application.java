e8ba457e61c809839fc01105906372bf36d4018d
Jo Myeong-geun <happy63590@gmail.com>
2024-11-11 22:43:06 +0900

    feat: 로직 실행 메서드 호출

--- src/main/java/store/Application.java
+++ src/main/java/store/Application.java
@@ -1,7 +1,21 @@
 package store;
 
+import store.controller.PurchaseController;
+import store.repository.ProductRepository;
+import store.repository.PromotionRepository;
+import store.service.PurchaseService;
+import store.view.InputView;
+import store.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        ProductRepository productRepository = new ProductRepository(new PromotionRepository());
+        PurchaseController purchaseController = new PurchaseController(
+                new PurchaseService(productRepository),
+                new InputView(),
+                new OutputView()
+        );
+
+        purchaseController.run();
     }
 }
